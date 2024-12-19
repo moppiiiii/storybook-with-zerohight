bbd85d661526e045d0146c0d06fd425c4e6fcded
moppiiiii <tnktmhssrc@gmail.com>
2024-12-19 17:32:30 +0900

    feat: storybook ref

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,15 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  refs: {
+    // 👇 Upper-case characters not supported in the refs key
+    "chromatic-published-storybook": {
+      // The title of your Storybook
+      title: "Design System Sample",
+      // The url provided by Chromatic when it was published
+      url: "https://moppiiiiis-components.chromatic.com",
+    },
+  },
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
