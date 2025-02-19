3438be57e61244e042af01c021ae00cf86904b2c
moppiiiii <tnktmhssrc@gmail.com>
2025-02-19 13:44:57 +0900

    badge

--- src/components/badge/Badge.component.stories.tsx
+++ src/components/badge/Badge.component.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import MailIcon from "@mui/icons-material/Mail";
 
 import BadgeComponent from "./Badge.component";
 
@@ -7,72 +6,6 @@ const meta: Meta<typeof BadgeComponent> = {
 	title: "Components/Badge",
 	tags: ["autodocs"],
 	component: BadgeComponent,
-	argTypes: {
-		badgeContent: {
-			description: "バッジに表示する内容",
-			table: {
-				type: { summary: "number" },
-				defaultValue: { summary: "0" },
-			},
-		},
-		color: {
-			control: { type: "select" },
-			options: ["primary", "secondary", "error", "info", "success", "warning"],
-			description: "バッジの色を選択",
-			table: {
-				type: { summary: "string" },
-				defaultValue: { summary: "primary" },
-			},
-		},
-		max: {
-			control: { type: "number" },
-			description: "表示する数値の最大値",
-			table: {
-				type: { summary: "number" },
-				defaultValue: { summary: "999" },
-			},
-		},
-		invisible: {
-			control: { type: "boolean" },
-			description: "バッジを非表示にするかどうか",
-			table: {
-				type: { summary: "boolean" },
-				defaultValue: { summary: "false" },
-			},
-		},
-		size: {
-			control: { type: "select" },
-			options: ["sm", "md", "lg"],
-			description: "バッジのサイズ",
-			table: {
-				type: { summary: "string" },
-				defaultValue: { summary: "md" },
-			},
-		},
-		showZero: {
-			control: { type: "boolean" },
-			description: "0 の場合でも表示するかどうか",
-			table: {
-				type: { summary: "boolean" },
-				defaultValue: { summary: "false" },
-			},
-		},
-		variant: {
-			control: { type: "select" },
-			options: ["standard", "dot"],
-			description: "バッジの表示スタイル (standard / dot)",
-			table: {
-				type: { summary: "string" },
-				defaultValue: { summary: "standard" },
-			},
-		},
-		children: {
-			description: "バッジが付与される要素",
-			table: {
-				type: { summary: "ReactNode" },
-			},
-		},
-	},
 };
 
 export default meta;
@@ -81,39 +14,15 @@ type Story = StoryObj<typeof BadgeComponent>;
 
 export const Default: Story = {
 	args: {
+		children: "Badge",
 		badgeContent: 5,
 	},
 };
 
-export const Small: Story = {
+export const OverMax: Story = {
 	args: {
-		badgeContent: 5,
-		color: "primary",
-		size: "sm",
-	},
-};
-
-export const Medium: Story = {
-	args: {
-		badgeContent: 5,
-		color: "primary",
-		size: "md",
-	},
-};
-
-export const Large: Story = {
-	args: {
-		badgeContent: 5,
-		color: "primary",
-		size: "lg",
-	},
-};
-
-export const IconBadge: StoryObj<typeof BadgeComponent> = {
-	args: {
-		badgeContent: 4,
-		color: "primary",
-		size: "md",
-		children: <MailIcon />,
+		children: "Badge",
+		max: 999,
+		badgeContent: 1000,
 	},
 };
--- src/components/badge/Badge.component.tsx
+++ src/components/badge/Badge.component.tsx
@@ -1,65 +1,26 @@
-import Badge from "@mui/material/Badge";
-import type { BadgeComponentProps } from "./type";
+import { Badge as MuiBadge } from "@mui/material";
+import type { BadgeProps as MuiBadgeProps } from "@mui/material";
 
-const BadgeComponent: React.FC<BadgeComponentProps> = ({
-	badgeContent,
-	color = "primary",
-	max = 999,
-	size = "md",
-	invisible = false,
-	showZero = false,
-	variant = "standard",
-	children,
+const Badge: React.FC<MuiBadgeProps> = ({
+  badgeContent,
+  children,
+  color = "primary",
+  variant = "standard",
+  ...props
 }) => {
-	let padding = "0 4px";
-	let badgeHeight = 22;
-	let fontSize = 9;
-	let borderRadius = 10;
+  const finalBadgeContent = variant === "dot" ? undefined : badgeContent;
 
-	switch (size) {
-		case "sm":
-			padding = "0 4px";
-			badgeHeight = 14;
-			fontSize = 6;
-			borderRadius = 2;
-			break;
-		case "md":
-			padding = "0 4px";
-			borderRadius = 10;
-			badgeHeight = 22;
-			fontSize = 12;
-			break;
-		case "lg":
-			padding = "0 4px";
-			badgeHeight = 30;
-			fontSize = 16;
-			break;
-	}
-
-	return (
-		<Badge
-			sx={{
-				"& .MuiBadge-badge": {
-					padding,
-					fontSize,
-					height: badgeHeight,
-					minWidth: badgeHeight,
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "center",
-					borderRadius
-				},
-			}}
-			badgeContent={badgeContent}
-			color={color}
-			max={max}
-			invisible={invisible}
-			showZero={showZero}
-			variant={variant}
-		>
-			{children}
-		</Badge>
-	);
+  return (
+    <MuiBadge
+      badgeContent={finalBadgeContent}
+      color={color}
+      max={999}
+      variant={variant}
+      {...props}
+    >
+      {children}
+    </MuiBadge>
+  );
 };
 
-export default BadgeComponent;
+export default Badge;
