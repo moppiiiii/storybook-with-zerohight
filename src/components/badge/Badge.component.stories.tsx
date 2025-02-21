d6afb862bb298251643c3c4a3da7bd56dbf2c6c5
moppiiiii <tnktmhssrc@gmail.com>
2025-02-21 09:17:09 +0900

    fix

--- src/components/badge/Badge.component.stories.tsx
+++ src/components/badge/Badge.component.stories.tsx
@@ -6,6 +6,13 @@ const meta: Meta<typeof BadgeComponent> = {
 	title: "Components/Badge",
 	tags: ["autodocs"],
 	component: BadgeComponent,
+	decorators: [
+		(Story) => (
+			<div style={{height: 100, paddingBlock: 20}}>
+				<Story />
+			</div>
+		)
+	]
 };
 
 export default meta;
@@ -14,14 +21,14 @@ type Story = StoryObj<typeof BadgeComponent>;
 
 export const Default: Story = {
 	args: {
-		children: "Badge",
+		children: <div style={{background: "purple"}}>BADGE</div>,
 		badgeContent: 5,
 	},
 };
 
 export const OverMax: Story = {
 	args: {
-		children: "Badge",
+		children: <div style={{background: "purple"}}>BADGE</div>,
 		badgeContent: 1000,
 	},
 };
